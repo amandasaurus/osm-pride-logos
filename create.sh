d1db13ce178b77c7d8883de891251ff489a1d03c
Amanda McCann <amanda@technomancy.org>
2022-05-28 16:16:37 +0200

    misc

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@
 set -o errexit
 set -o nounset
 
-cd $(dirname $0)
+cd "$(dirname $0)" || exit
 
 SIZES="500 100"
 
@@ -11,23 +11,31 @@ for FLAG in ./flags/*.svg ; do
     NAME=${NAME##.*/}
     NAME=${NAME%%.svg}
     OUTPUT=OSM_white_bg_with_small_border.${NAME}
-    sed "s|FLAGFLAGFLAG|${FLAG}|" ./templates/logo_base.template.svg | gzip > OSM_white_bg_with_small_border.${NAME}.svgz
-    sed "s|FLAGFLAGFLAG|${FLAG}|" ./templates/logo_base_nb.template.svg | gzip > OSM_white_bg_with_small_border.nb.${NAME}.svgz
-    inkscape --without-gui --file=OSM_white_bg_with_small_border.${NAME}.svgz --export-dpi 300 --export-pdf ${OUTPUT}.pdf --export-background white --export-margin 3
-    inkscape --without-gui --file=OSM_white_bg_with_small_border.nb.${NAME}.svgz --export-dpi 300 --export-pdf ${OUTPUT}.nb.pdf --export-background white --export-margin 3
+    sed "s|FLAGFLAGFLAG|${FLAG}|" ./templates/logo_base.template.svg > OSM_white_bg_with_small_border.${NAME}.svg
+    cat OSM_white_bg_with_small_border.${NAME}.svg | gzip > OSM_white_bg_with_small_border.${NAME}.svgz
+    sed "s|FLAGFLAGFLAG|${FLAG}|" ./templates/logo_base_nb.template.svg > OSM_white_bg_with_small_border.nb.${NAME}.svg
+	cat  OSM_white_bg_with_small_border.nb.${NAME}.svg | gzip > OSM_white_bg_with_small_border.nb.${NAME}.svg
+    inkscape OSM_white_bg_with_small_border.${NAME}.svgz --export-dpi 300 --export-type="pdf" --export-background white --export-margin 3
+    inkscape OSM_white_bg_with_small_border.nb.${NAME}.svgz --export-dpi 300 --export-type="pdf" --export-background white --export-margin 3
+
+    inkscape OSM_white_bg_with_small_border.${NAME}.svgz --export-dpi 300 --export-type="pdf" --export-filename=tmp.pdf
+    inkscape tmp.pdf --export-type="svg" --export-filename=OSM_white_bg_with_small_border.${NAME}.svg
+	rm tmp.pdf
+    cat OSM_white_bg_with_small_border.${NAME}.svg | gzip > OSM_white_bg_with_small_border.${NAME}.svgz
+
     for SIZE in $SIZES ; do
-        inkscape --without-gui --file=OSM_white_bg_with_small_border.${NAME}.svgz -w $SIZE -h $SIZE --export-png ${OUTPUT}.${SIZE}.png --export-background white
-        inkscape --without-gui --file=OSM_white_bg_with_small_border.nb.${NAME}.svgz -w $SIZE -h $SIZE --export-png ${OUTPUT}.${SIZE}.nb.png --export-background white
+        inkscape OSM_white_bg_with_small_border.${NAME}.svgz -w $SIZE -h $SIZE --export-type="png" --export-filename=${OUTPUT}.${SIZE}.png --export-background white
+        inkscape OSM_white_bg_with_small_border.nb.${NAME}.svgz -w $SIZE -h $SIZE --export-type="png" --export-filename=${OUTPUT}.${SIZE}.nb.png --export-background white
         pngquant -f --ext .png 128 ${OUTPUT}.${SIZE}.png
         pngquant -f --ext .png 128 ${OUTPUT}.${SIZE}.nb.png
     done
-    echo $NAME
+    echo "Done $NAME"
 done
 
 # alas this are broken, flag is all stretched
 rm *.leather_bdsm.*png *bear*.png *intersex*.png
 
-exiftool -overwrite_original -XMP-dc:Rights="This work is licensed to the public under the Creative Commons Attribution-ShareAlike license http://creativecommons.org/licenses/by-sa/4.0/" -xmp:usageterms="This work is licensed to the public under the Creative Commons Attribution-ShareAlike license http://creativecommons.org/licenses/by-sa/4.0/" -XMP-cc:license="http://creativecommons.org/licenses/by-sa/4.0/" -XMP-cc:AttributionName="R McCann" -XMP-cc:AttributionURL="www.technomancy.org" *.png
+exiftool -overwrite_original -XMP-dc:Rights="This work is licensed to the public under the Creative Commons Attribution-ShareAlike license http://creativecommons.org/licenses/by-sa/4.0/" -xmp:usageterms="This work is licensed to the public under the Creative Commons Attribution-ShareAlike license http://creativecommons.org/licenses/by-sa/4.0/" -XMP-cc:license="http://creativecommons.org/licenses/by-sa/4.0/" -XMP-cc:AttributionName="Amanda McCann" -XMP-cc:AttributionURL="www.technomancy.org" *.png
 
 
 rm -rf montage_images
